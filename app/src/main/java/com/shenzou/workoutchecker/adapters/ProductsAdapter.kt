7038ba25f5a0756c89865e08604f915a4f520f70
shenzou <alexandre.shenouda@edu.devinci.fr>
2020-10-14 11:43:20 +0200

    Changed design of the food item selector

--- app/src/main/java/com/shenzou/workoutchecker/adapters/ProductsAdapter.kt
+++ app/src/main/java/com/shenzou/workoutchecker/adapters/ProductsAdapter.kt
@@ -1,15 +1,21 @@
 package com.shenzou.workoutchecker.adapters
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.shenzou.workoutchecker.R
 import com.shenzou.workoutchecker.objects.ProductData
 import com.shenzou.workoutchecker.objects.ProductElement
 import kotlinx.android.synthetic.main.item_food_list.view.*
+import kotlin.math.roundToInt
 
 class ProductsAdapter(val items: ArrayList<ProductElement>, val ctx: Context): RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
 
@@ -28,12 +34,23 @@ class ProductsAdapter(val items: ArrayList<ProductElement>, val ctx: Context): R
 
         var energy: Double = items[position].productData.product.nutriments.energy?.div(100) ?: 0.0
         energy = energy.times(quantityInt)
+        energy = ((energy * 100).roundToInt() /100.00)
 
         var proteins: Double = items[position].productData.product.nutriments.proteins?.div(100) ?: 0.0
         proteins = proteins.times(quantityInt)
+        proteins = ((proteins * 100).roundToInt() /100.00)
 
         var glucides: Double = items[position].productData.product.nutriments.carbohydrates?.div(100) ?: 0.0
         glucides = glucides.times(quantityInt)
+        glucides = ((glucides * 100).roundToInt() /100.00)
+
+        var fat: Double = items[position].productData.product.nutriments.fat?.div(100) ?: 0.0
+        fat = fat.times(quantityInt)
+        fat = ((fat * 100).roundToInt() /100.00)
+
+        var saturated: Double = items[position].productData.product.nutriments.saturated_fat?.div(100) ?: 0.0
+        saturated = fat.times(quantityInt)
+        saturated = ((saturated * 100).roundToInt() /100.00)
 
         if(items[position].productData.product.product_name_fr != ""){
             holder.name?.text = items[position].productData.product.product_name_fr
@@ -50,6 +67,27 @@ class ProductsAdapter(val items: ArrayList<ProductElement>, val ctx: Context): R
         holder.proteines?.text = proteins.toString()
         holder.glucides?.text = glucides.toString()
         holder.quantity?.text = items[position].quantity.toString()
+        holder.fat?.text = fat.toString()
+        holder.saturated?.text = saturated.toString()
+
+        holder.progressCalories?.max = 2500
+        holder.progressCalories?.progress = energy.toInt()
+        holder.progressCalories?.progressDrawable!!.setColorFilter(this.ctx.resources.getColor(R.color.progressBarBlue), PorterDuff.Mode.SRC_IN)
+
+        holder.progressProteins?.max = 120
+        holder.progressProteins?.progress = proteins.toInt()
+        holder.progressProteins?.progressDrawable!!.setColorFilter(this.ctx.resources.getColor(R.color.progressBarGreen), PorterDuff.Mode.SRC_IN)
+
+        holder.progressCarbs?.max = 1300
+        holder.progressCarbs?.progress = glucides.toInt()
+        holder.progressCarbs?.progressDrawable!!.setColorFilter(this.ctx.resources.getColor(R.color.progressBarRed), PorterDuff.Mode.SRC_IN)
+
+        holder.progressFat?.max = 100
+        holder.progressFat?.progress = fat.toInt()
+        holder.progressFat?.progressDrawable!!.setColorFilter(this.ctx.resources.getColor(R.color.progressBarYellow), PorterDuff.Mode.SRC_IN)
+
+
+
 
         Glide.with(ctx)
             .load(items[position].productData.product.image_front_small_url)
@@ -63,6 +101,12 @@ class ProductsAdapter(val items: ArrayList<ProductElement>, val ctx: Context): R
         val glucides = view.glucides
         val iconProduct = view.iconProduct
         val quantity = view.quantity
+        val fat = view.fat
+        val saturated = view.saturated
+        val progressCalories = view.progressBarCalories
+        val progressProteins = view.progressBarProteins
+        val progressCarbs = view.progressBarCarbs
+        val progressFat = view.progressBarFat
 
         init{
             view.setOnClickListener {
